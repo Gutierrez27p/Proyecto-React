e5699925c9e6d4e7d8d7c7b4dd5cec87237ac697
Pablo Gutierrez <pablo.guty.2008@gmail.com>
2022-12-10 12:53:47 -0300

    moficicaciones en item detail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -2,18 +2,12 @@ import { Link } from 'react-router-dom'
 
 export const ItemDetail = ({ item }) => {
 
-    const { image, size, name, price, description} = item
 
     return (
         <div className="cardDetail row">
             <div className="col-md-4">
-                <img src={image} alt={name} className="img-fluid" />
             </div>
             <div className="col-md-12">
-                <h1>{name}</h1>
-                <p>{description}</p>
-                <p>{size}</p>
-                <p><b>${price}</b></p>
             </div>
         </div>
     )
